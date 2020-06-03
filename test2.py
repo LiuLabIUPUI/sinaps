fb27ec0cda9e14660191d08fa1deeca9ebef0c9d
Clayton <cwseitz@indiana.edu>
2020-06-03 11:06:21 -0400

    update test2.py

--- test2.py
+++ test2.py
@@ -1,5 +1,6 @@
 from skimage.io import imread, imsave
 from skimage.util import img_as_ubyte
+from copy import deepcopy
 from sinaps.brownian import build_traj2
 from sinaps.util import *
 from sinaps.photon import *
@@ -10,7 +11,8 @@ import matplotlib.pyplot as plt
 #~~~~~~~params~~~~~~~~~~
 #
 
-path = '/home/cwseitz/Desktop/test.tif'
+path1 = '/home/cwseitz/Desktop/test.tif'
+path2 = '/home/cwseitz/Desktop/test2.tif'
 shape = (100,100)
 nparticles=16
 nframes=10
@@ -26,12 +28,20 @@ origins = lattice_size*origins
 traj_df = build_traj2(nparticles=nparticles, nframes=nframes, origins=origins, dcoeff=.1)
 
 #
-#~~~~~~~build the movie~~~~~~~~~~
+#~~~~~~~build noisy and noiseless movies~~~~~~~~~~
 #
 
-traj_df = add_photon_stats(traj_df, add_noise=False); print(traj_df)
-movie = add_psfs_batch(movie, traj_df)
-movie = add_noise_batch(movie, sigma_dark=0.1)
-movie = normalize_by_frame(movie)
-movie = img_as_ubyte(movie)
-imsave(path, movie)
+traj_df = add_photon_stats(traj_df, add_noise=False)
+no_noise = add_psfs_batch(movie, traj_df)
+
+noise = deepcopy(no_noise)
+noise = add_noise_batch(noise, sigma_dark=0.1)
+
+no_noise = normalize_by_frame(no_noise)
+no_noise = img_as_ubyte(no_noise)
+
+noise = normalize_by_frame(noise)
+noise = img_as_ubyte(noise)
+
+imsave(path1, noise)
+imsave(path2, no_noise)
